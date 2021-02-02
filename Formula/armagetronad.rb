794555c09244177944ac01aefef59af91b034e6e
Christian Mäder <mail@cimnine.ch>
2021-02-02 21:12:38 +0100

    Add trunk build

---
+++ Formula/armagetronad.rb
@@ -0,0 +1,42 @@
+class Armagetronad < Formula
+  desc "Armagetron is a multiplayer game in 3d that attempts to emulate and expand on the lightcycle sequence from the movie Tron. It's an old school arcade game slung into the 21st century. Highlights include a customizable playing arena, HUD, unique graphics, and AI bots. For the more advanced player there are new game modes and a wide variety of physics settings to tweak as well."
+  homepage "http://www.armagetronad.org/"
+  license "GPL-2.0-only"
+  #url "https://gitlab.com/armagetronad/armagetronad/-/archive/v0.4.0.0.0/armagetronad-v0.4.0.0.0.tar.bz2"
+  #sha256 "09df905124667ea2bda7c85c4696b88684e18af1d31d6569e537d84a6166088b"
+  head "https://gitlab.com/armagetronad/armagetronad.git", :branch => "trunk"
+  revision 0
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "boost" => :build
+  depends_on "ftgl"
+  depends_on "libpng"
+  depends_on "protobuf"
+  depends_on "sdl2"
+
+  uses_from_macos "python"
+
+  def install
+    system "./bootstrap.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-zthreadtest",
+                          "--disable-uninstall",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      You need to run Armagtron Advanced from the Terminal as 'armagetronad'.
+      There is currently no shortcut in the Applications directory.
+      Neither is a cask available.
+    EOS
+  end
+
+  test do
+    system "#{bin}/armagetronad", "-v"
+  end
+end
--- README.md
+++ README.md
@@ -10,6 +10,10 @@ In particular:
 * `brew install --HEAD cimnine/armagetronad/armagetronad@0.2.8`
 * `brew install --HEAD cimnine/armagetronad/armagetronad@0.2.9`
 
+Someoday maybe, but does not yet build or start without crashing:
+
+* `brew install --HEAD cimnine/armagetronad/armagetronad`
+
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
